eaab64f67456d8a343eefcf52b12050106f900ea
JeremieAucher <jeremie.aucher@gmail.com>
2021-07-20 17:05:12 +0200

    1er commit

---
+++ P8_06_bootstrap-emr.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo python3 -m pip install -U setuptools
+sudo python3 -m pip install -U pip
+sudo python3 -m pip install pillow
+sudo python3 -m pip install pandas==1.2.5
+sudo python3 -m pip install pyarrow
+sudo python3 -m pip install s3fs
+sudo python3 -m pip install fsspec
